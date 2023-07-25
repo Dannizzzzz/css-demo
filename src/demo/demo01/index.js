d78e16e07a734d8178802d63c6dde41e38639955
Dannizzzzz <oliviazhang096@gmail.com>
2023-07-25 15:04:36 +0930

    demo01-Horizontal Scrolling

---
+++ src/demo/demo01/index.js
@@ -0,0 +1,37 @@
+import { useEffect } from 'react';
+import './index.css';
+
+function Demo() {
+  useEffect(()=>{
+    function scrollH(e) {
+      e.preventDefault();
+      document.querySelector('.container').scrollLeft += e.deltaY;
+    }
+    if (document.querySelector('.container').addEventListener) {
+      document.querySelector('.container').addEventListener('mousewheel', scrollH, false);
+      document.querySelector('.container').addEventListener('DOMMouseScroll', scrollH, false);
+    }
+  },[])
+  
+  return (
+    <div className="container">
+      <div className="item">
+        <h1>OUR WORK</h1>
+      </div>
+      <div className="item">
+        <img src="images/demo01/test1.jpg" alt="test1" />
+      </div>
+      <div className="item">
+        <img src="images/demo01/test2.jpg" alt="test2" />
+      </div>
+      <div className="item">
+        <img src="images/demo01/test3.jpg" alt="test3" />
+      </div>
+      <div className="item">
+        <img src="images/demo01/test4.jpg" alt="test4" />
+      </div>
+    </div>
+  );
+}
+
+export default Demo;
